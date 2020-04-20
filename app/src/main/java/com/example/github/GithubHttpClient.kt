9d9631884bf3a9d7cc276723feb37065fd7b16c2
vorabhol chuaybhan <max003003003@gmail.com>
2020-04-20 12:28:48 +0700

    Remove unused code

--- app/src/main/java/com/example/github/GithubHttpClient.kt
+++ app/src/main/java/com/example/github/GithubHttpClient.kt
@@ -5,10 +5,6 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 
 class GithubHttpClient : IGithubHttpClient {
 	override suspend  fun retrieveRepo(): GitHubService {
-//		val gson = GsonBuilder()
-//			.registerTypeAdapter(GithubRepo::class.java, GithubRepo())
-//			.create()
-
 		val retrofit = Retrofit.Builder()
 			.baseUrl("https://api.github.com")
 			.addConverterFactory(ScalarsConverterFactory.create())
