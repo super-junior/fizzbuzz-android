0882f14e872b653b10614ed43c5108129c265c95
vorabhol chuaybhan <max003003003@gmail.com>
2020-04-28 22:35:07 +0700

    Add search detail fragment

--- app/src/main/java/com/example/github/GitHubActivity.kt
+++ app/src/main/java/com/example/github/GitHubActivity.kt
@@ -1,13 +1,8 @@
 package com.example.github
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.lifecycleScope
 import com.example.R
-import kotlinx.android.synthetic.main.github_activity.*
-import kotlinx.coroutines.launch
 
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -21,7 +16,7 @@ class GitHubActivity: AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.github_activity)
-		val displayFragment = display()
+		val displayFragment = SearchResultListFragment()
 		fragmentTransaction.add(R.id.fragment_container, displayFragment)
 		fragmentTransaction.commit()
 
